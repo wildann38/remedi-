b198853296b57239c471f3ef493e52ac01b2cec5
unknown <abdillahhafidz19@gmail.com>
2024-06-12 09:53:08 +0700

    remidi

---
+++ remidi.py
@@ -0,0 +1,16 @@
+#buatlah list dengan nama apk desain isi dengan nama apk desain
+list = ["Canva", "Corel", "Acrobatpro", "Figma", "Photoshop", "XD", "Premiere Pro", "After Effects","Lightroom"]
+print= ('angka ke 7') 
+
+#buat dictionary dengan nama english sejumlah 10 data
+english_languange = {'Name':'Nama', 'Age':'umur', 'Class':'kelas', 'district':'daerah', 'cat':'kucing', 'table':'meja', 'chair':'kursi', 'iron':'besi', 'fire':'api', 'water':'air' }
+print=("'Name':'Nama', 'Age':'umur', 'Class':'kelas', 'district':'daerah', 'cat':'kucing', 'table':'meja', 'chair':'kursi', 'iron':'besi', 'fire':'api', 'water':'air'"  )
+
+#mendefinisikan fungsi dengan nama trapesium
+#buatlah parameter a dengan nilai 5, parameter b nilai 7, parameter t nilai 10 
+#masukkan rumus trapesium
+def trapesium (a=5, b=7, t=10):
+ luas = 0.5 * (a + b) * t
+ luas = 0.5 * (5 + 7) * 10
+
+ print = (0.5 * (5 + 7) * 10)
